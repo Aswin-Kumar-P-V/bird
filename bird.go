52a1e2ce67409c7b84209117c0d4b47602ce437e
Aswin <aswinkumarpv239@gmail.com>
2023-06-12 11:03:30 +0530

    DONE

--- bird.go
+++ bird.go
@@ -1,9 +1,15 @@
 package bird
 
+import noofbirds "github.com/Aswin-Kumar-P-V/noOfBirds"
+
 func Chirp() string {
 	return "chirp"
 }
 
 func Chirps() string {
 	return "chirp..chirp"
+}
+
+func BirdsNo() int {
+	return noofbirds.Noofbirds()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Aswin-Kumar-P-V/bird
 
 go 1.20
+
+require github.com/Aswin-Kumar-P-V/noOfBirds v0.0.0-20230612052504-4af01ac0dfd3
